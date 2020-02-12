3bc3a378d24b657290cabb4c07483894fa2d18b7
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-12 11:13:10 +0100

    something

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
+
 const imageRouter = require("./image/router");
 const loginRouter = require("./auth/router");
 const userRouter = require("./user/router");
